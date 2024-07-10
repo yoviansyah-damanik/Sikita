df790be1e6f16dcf10baa858e76cc17271055431
Yoviansyah Rizki Pratama <yoviansyahrizkypratama@gmail.com>
2024-07-10 23:39:06 +0700

    Update GuidanceType.php

--- app/Models/GuidanceType.php
+++ app/Models/GuidanceType.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 #[ScopedBy([LastOrderScope::class])]
 class GuidanceType extends Model
@@ -17,5 +18,10 @@ class GuidanceType extends Model
     public function group(): BelongsTo
     {
         return $this->belongsTo(GuidanceGroup::class);
+    }
+
+    public function students(): HasManyThrough
+    {
+        return $this->hasManyThrough(Student::class, Guidance::class, 'id', 'id', 'guidance_type_id', 'student_id');
     }
 }
